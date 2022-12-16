2bad4861e084a132b2f43d5a9faa174ccad1d902
altangerel.d <altangerel.d@ddishtv.mn>
2022-12-16 17:46:11 +0800

    player fix

--- assets/frontend/flixer/neon.js
+++ assets/frontend/flixer/neon.js
@@ -196,14 +196,14 @@ function nback(){
     console.log('backbtn')
     let current = dp.video.currentTime
     dp.seek(current >= 10 ? current - 10 : 0);
-    dp.play();
+    dp.toggle();
 }
 
 function nskip(){
     console.log('skipbtn')
     let current = dp.video.currentTime
     dp.seek(current + 10);
-    dp.play();
+    dp.toggle();
 }
 
 function nprev(){
