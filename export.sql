21454e3b11bd712c208c4778592f740edc577e04
altangerel.d <altangerel.d@ddishtv.mn>
2022-12-22 16:45:20 +0800

    new setup

--- export.sql
+++ export.sql
@@ -19,10 +19,6 @@
 -- Table structure for table `actor`
 --
 
-CREATE DATABASE IF NOT EXISTS vflix
-CHARACTER SET utf8mb4
-COLLATE utf8mb4_unicode_ci
-
 DROP TABLE IF EXISTS `actor`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
