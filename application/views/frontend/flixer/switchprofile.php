bfb21c00da734408dad4aae3303f3ed55ede4b72
altangerel.d <altangerel.d@ddishtv.mn>
2022-12-24 20:34:35 +0800

    player fix

--- application/views/frontend/flixer/switchprofile.php
+++ application/views/frontend/flixer/switchprofile.php
@@ -16,52 +16,16 @@
 		</a>
 	</div>
 	<div style="clear: both; text-align: center; padding-top: 100px;">
-		<h1>Who's watching?</h1>
+		<h1>Өө, сорри!</h1>
+		<h5>Яг одоо энэ аккаунтаар өөр төхөөрөмжөөс холбогдсон байна. Давхар үзэх боломжгүй.</h5>
 		<table align="center" style="background-color: #141414;">
 			<tr>
 				<td>
-					<a href="<?php echo base_url();?>index.php?browse/doswitch/user1" class="profile_switcher" style="text-decoration: none;">	
 					<img src="<?php echo base_url();?>/assets/global/thumb1.png" class="profile_switcher_img" />
-					<br>
-					<?php echo $this->crud_model->get_username_of_user('user1');?>
-					</a>
 				</td>
-				<?php
-					if (1 == 2 && $current_plan_id == 2 || $current_plan_id == 3):
-					?>
-				<td>
-					<a href="<?php echo base_url();?>index.php?browse/doswitch/user2" class="profile_switcher" style="text-decoration: none;">	
-					<img src="<?php echo base_url();?>/assets/global/thumb2.png" class="profile_switcher_img" />
-					<br>
-					<?php echo $this->crud_model->get_username_of_user('user2');?>
-					</a>
-				</td>
-				<?php endif;?>
-				<?php
-					if (1 == 2 && $current_plan_id == 3):
-					?>
-				<td>
-					<a href="<?php echo base_url();?>index.php?browse/doswitch/user3" class="profile_switcher" style="text-decoration: none;">	
-					<img src="<?php echo base_url();?>/assets/global/thumb3.png" class="profile_switcher_img" />
-					<br>
-					<?php echo $this->crud_model->get_username_of_user('user3');?>
-					</a>
-				</td>
-				<?php endif;?>
-				<?php
-					if (1 == 2 && $current_plan_id == 3):
-					?>
-				<td>
-					<a href="<?php echo base_url();?>index.php?browse/doswitch/user4" class="profile_switcher" style="text-decoration: none;">	
-					<img src="<?php echo base_url();?>/assets/global/thumb4.png" class="profile_switcher_img" />
-					<br>
-					<?php echo $this->crud_model->get_username_of_user('user4');?>
-					</a>
-				</td>
-				<?php endif;?>
 			</tr>
 		</table>
 		<br>
-		<a href="<?php echo base_url();?>index.php?browse/manageprofile" class="profile_manage">MANAGE PROFILES</a>
+		<a href="<?php echo base_url();?>index.php?home/landing2" class="profile_manage">Буцах</a>
 	</div>
 </div>
